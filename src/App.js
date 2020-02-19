bbfe6fcb20b24717ae138cbaa2a51fcca76587cf
Daniel Bright <brahyt@gmail.com>
2020-02-19 15:30:55 -0800

    added FoodWindow

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import FoodWindow from './FoodWindow'
 
 function App() {
   return (
     <div className="App">
+      <FoodWindow />
     </div>
   );
 }
---
+++ src/FoodWindow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FoodWindow.css';
+
+function FoodWindow() {
+  return (
+    <div className="FoodWindow">
+      <p>Food Window</p>
+    </div>
+  );
+}
+
+export default FoodWindow;
