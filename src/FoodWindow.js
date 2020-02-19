66041ed72d45a344a2097d6f8f175120708cec95
Daniel Bright <brahyt@gmail.com>
2020-02-19 15:41:31 -0800

    added FoodWindow

--- src/FoodWindow.js
+++ src/FoodWindow.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './FoodWindow.css';
 
 function FoodWindow() {
+  const [data, setData] = React.useState()
+
   return (
     <div className="FoodWindow">
       <p>Food Window</p>
