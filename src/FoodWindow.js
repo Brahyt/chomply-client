ad6c32b889b115043811b602ec5d2a96ebf9f9d7
Daniel Bright <brahyt@gmail.com>
2020-02-21 12:07:56 -0800

    added fetch string for fetchRecipie

--- src/FoodWindow.js
+++ src/FoodWindow.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import './FoodWindow.css';
 import Button from './Button';
+import RecipeCard from './RecipeCard';
 
 function FoodWindow() {
-  const foodReducer = (state, action) => {
-  
-  }
-  const [state, dispatch] = React.useReducer(
-    foodReducer,
-    {loading: true, error: null, data: null}
-  )
-  const fetchRecipie = () => {
-    //fetch recipie
-    //
-  }
+  const fetchRecipe = url => {
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        console.log(data);
+      });
+  };
+  const foodReducer = (state, action) => {};
+  const [state, dispatch] = React.useReducer(foodReducer, {
+    loading: true,
+    error: null,
+    data: null,
+    ingrediant: [],
+  });
+  const createIngredientList = () => {
+    const list = state.ingrediant;
+    return list.join("+").toLowerCase()
+  };
+  fetchRecipe(
+    `${process.env.REACT_APP_API_URL}?q=${createIngredientList(
+      state.ingrediant,
+    )}&app_key=${process.env.REACT_APP_API_KEY}`,
+  );
   return (
     <div className="FoodWindow">
+      <RecipeCard />
       <p>Food Window</p>
-      <Button 
-        action="submit"
-      />
+      <Button action="submit" />
     </div>
   );
 }
---
+++ src/RecipeCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RecipeCard.css';
+
+function RecipeCard() {
+  return (
+    <div className="RecipeCard">
+      <p>RecipeCard</p>
+    </div>
+  );
+}
+
+export default RecipeCard;
