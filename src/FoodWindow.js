e29cdf1bc9886460565a1cd098a5fe49feba5402
Daniel Bright <brahyt@gmail.com>
2020-02-21 12:13:14 -0800

    te

--- src/FoodWindow.js
+++ src/FoodWindow.js
@@ -4,6 +4,7 @@ import Button from './Button';
 import RecipeCard from './RecipeCard';
 
 function FoodWindow() {
+
   const fetchRecipe = url => {
     fetch(url)
       .then(resp => resp.json())
@@ -11,22 +12,29 @@ function FoodWindow() {
         console.log(data);
       });
   };
+
   const foodReducer = (state, action) => {};
+
   const [state, dispatch] = React.useReducer(foodReducer, {
     loading: true,
     error: null,
     data: null,
     ingrediant: [],
   });
+
+  /*Create string from state.ingrediant for API request*/
   const createIngredientList = () => {
     const list = state.ingrediant;
     return list.join("+").toLowerCase()
   };
+
+  /*Call Fetch from url on component load*/
   fetchRecipe(
     `${process.env.REACT_APP_API_URL}?q=${createIngredientList(
       state.ingrediant,
     )}&app_key=${process.env.REACT_APP_API_KEY}`,
   );
+
   return (
     <div className="FoodWindow">
       <RecipeCard />
