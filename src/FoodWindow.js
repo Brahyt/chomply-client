221363d830a7008d639773cd5a25b60dc4edd45b
Daniel Bright <brahyt@gmail.com>
2020-02-21 13:48:00 -0800

    fetch working

--- src/FoodWindow.js
+++ src/FoodWindow.js
@@ -4,13 +4,12 @@ import Button from './Button';
 import RecipeCard from './RecipeCard';
 
 function FoodWindow() {
-
   /*Quick fetch for testing purposes*/
   const fetchRecipe = url => {
     fetch(url)
       .then(resp => {
         console.log(resp)
-        return resp.json()
+        return resp.ok ? resp.json() : console.log(resp.status);
       })
       .then(data => {
         console.log(data);
@@ -23,25 +22,26 @@ function FoodWindow() {
     loading: true,
     error: null,
     data: null,
-    ingrediant: [],
+    ingredient: ['chicken', 'cumin', 'cream', 'curry'],
   });
 
-  /*Create string from state.ingrediant for API request*/
+  /*Create string from state.ingredient for API request*/
   const createIngredientList = () => {
-    const list = state.ingrediant;
-    return list.join(",+").toLowerCase()
+    const list = state.ingredient;
+    return list.join().toLowerCase();
   };
 
   /*Call Fetch from url on component load*/
   fetchRecipe(
     `${process.env.REACT_APP_API_URL}?ingredients=${createIngredientList(
-      state.ingrediant,
-    )}&app_key=${process.env.REACT_APP_API_KEY}`,
+      state.ingredient,
+    )}&apiKey=${process.env.REACT_APP_API_KEY}`,
   );
 
   return (
     <div className="FoodWindow">
       <RecipeCard />
+      <p>{createIngredientList(state.ingredient)}</p>
       <p>Food Window</p>
       <Button action="submit" />
     </div>
