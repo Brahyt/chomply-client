eb267221905d489e37d6f8f2efd8740924745d86
Daniel Bright <brahyt@gmail.com>
2020-02-24 15:21:02 -0800

    afk

--- src/IngredientList.js
+++ src/IngredientList.js
@@ -3,24 +3,28 @@ import './IngredientList.css';
 import {v4 as uuidv4} from 'uuid';
 
 function IngredientList(props) {
-
+  /*Build a list of ingredients
+   * 5 tsp - cumin
+   * 2 tbsp - butter
+   * 1 lb - chicken
+   * */
   const createList = () => {
-    const {ingredients} = props
-    return ingredients.map((ingredient)=>{
-      const {name, amount, unitShort} = ingredient
+    const {ingredients} = props;
+    return ingredients.map(ingredient => {
+      const {name, amount, unitShort} = ingredient;
       return (
-        <li key={uuidv4()}>{amount} {unitShort} - {name}</li>
-      )
-    })
-  }
+        <li key={uuidv4()}>
+          {amount} {unitShort} - {name}
+        </li>
+      );
+    });
+  };
 
-  console.log(props)
+  console.log(props);
   return (
-      <div className="IngredientList">
-        <ul>
-          {createList()}
-        </ul>
-      </div>
+    <div className="IngredientList">
+      <ul>{createList()}</ul>
+    </div>
   );
 }
 
