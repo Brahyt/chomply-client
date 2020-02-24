4f1786ed4372dc116daed2692fda6bba86dd9a3a
Daniel Bright <brahyt@gmail.com>
2020-02-23 22:52:43 -0800

    added enzyme

--- src/FoodWindow.test.js
+++ src/FoodWindow.test.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import FoodWindow from './FoodWindow';
 
+const defaultProps = {data: {title: "test title"}}
 test('renders learn react link', () => {
-  const { getByText } = render(<FoodWindow />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(
+    <FoodWindow 
+      data={{data: {title: "test title"}}}
+    />
+);
+  const linkElement = getByText(/test title/i);
   expect(linkElement).toBeInTheDocument();
 });
