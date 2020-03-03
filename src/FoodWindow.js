3e77cbffa126f994b362b4db29f81bb1ec9f6330
Daniel Bright <brahyt@gmail.com>
2020-03-03 09:52:25 -0800

    removed hanging vars

--- src/FoodWindow.js
+++ src/FoodWindow.js
@@ -3,10 +3,8 @@ import './FoodWindow.css';
 import RecipeCard from './RecipeCard';
 import Form from './Form';
 import {Box} from '@material-ui/core';
-import {useTheme} from '@material-ui/core/styles';
 
 function FoodWindow(props) {
-  const theme = useTheme();
   const ingredientRef = React.useRef();
 
   /*Quick fetch for testing purposes*/
--- src/Form.js
+++ src/Form.js
@@ -3,29 +3,29 @@ import './Form.css';
 import {
   Box,
   Button,
-  Input,
+  TextField,
   FormControl,
   FormHelperText,
 } from '@material-ui/core';
 
+
 function Form(props) {
   const {ingredientRef, submit} = props;
   return (
-    <Box marginY="5%" width='100%'>
+    <Box marginY="5%" width="100%" >
       <form onSubmit={submit}>
-        <FormControl fullWidth={true}>
-          <FormHelperText
-            id="ingredients-helper">
+        <FormControl fullWidth>
+          <FormHelperText id="ingredients-helper" variant="outlined">
             What do you want to cook with today?
           </FormHelperText>
-          <Input
+          <TextField
             id="ingredients"
             name="ingredient"
             type="text"
             placeholder="chicken, cream..."
             variant="outlined"
             autoFocus
-            width="100%"
+            fullwidth
             inputRef={ingredientRef}
           />
           <Box marginY="3%" width="100%">
