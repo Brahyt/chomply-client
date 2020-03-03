d5c790e1ddb75fdd1b551427fba4960dedb5ae83
Daniel Bright <brahyt@gmail.com>
2020-03-03 09:29:40 -0800

    pretextfield

--- src/Form.js
+++ src/Form.js
@@ -1,39 +1,46 @@
 import React from 'react';
 import './Form.css';
-import {Button, Input, FormControl, FormHelperText} from '@material-ui/core';
+import {
+  Box,
+  Button,
+  Input,
+  FormControl,
+  FormHelperText,
+} from '@material-ui/core';
 
 function Form(props) {
   const {ingredientRef, submit} = props;
   return (
-    <form 
-      onSubmit={submit}
-    >
-      <FormControl
-       fullWidth={true}
-      >
+    <Box marginY="5%" width='100%'>
+      <form onSubmit={submit}>
+        <FormControl fullWidth={true}>
           <FormHelperText
-            style={{margin: [10,10,0,10]}}
-            id="ingredients-helper"
-          >What do you want to cook with today?</FormHelperText>
+            id="ingredients-helper">
+            What do you want to cook with today?
+          </FormHelperText>
           <Input
-            style={{margin: [0,10,10,10]}}
             id="ingredients"
             name="ingredient"
             type="text"
             placeholder="chicken, cream..."
+            variant="outlined"
             autoFocus
-            width='100%'
+            width="100%"
             inputRef={ingredientRef}
           />
-          <Button
-            style={{margin: 10}}
-            variant="contained"
-            color="secondary"
-            size="large"
-            onClick={submit}
-          >Chomp</Button>
+          <Box marginY="3%" width="100%">
+            <Button
+              fullWidth
+              variant="contained"
+              color="secondary"
+              size="large"
+              onClick={submit}>
+              Chomp
+            </Button>
+          </Box>
         </FormControl>
       </form>
+    </Box>
   );
 }
 
